755b3df92784cb0db15c92b3d7922ff2be377017
SusanaLauH <susanalauhou@hotmail.com>
2013-09-06 01:02:10 -0700

    Initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Panamamentors::Application.config.secret_token = 'ac8cedd0c7523912ce085520cd0b95ceacb6b39df162b42575dcb06c66e4970dff8abe13dbe0a12ec91e677fc460ff9ad76619a9fcc855afed2210afe5fbb11d'
